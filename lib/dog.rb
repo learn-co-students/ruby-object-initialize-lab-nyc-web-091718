3ddfa818c3e21ba57e15f509c313b5a69d7b6b73
github username <github email address>
2018-09-18 19:33:17 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+require 'pry'
+
+class Dog
+attr_accessor :name, :breed
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class Person
+  def initialize(name)
+    @name = name
+  end
+  def name= (name)
+    @name= name
+  end
+  def name
+    @name
+  end
+end
